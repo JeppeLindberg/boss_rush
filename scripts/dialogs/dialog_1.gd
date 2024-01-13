b58a1dcba253980a332e6af2b78b5e6fb7f8c268
unknown <jepp3441@gmail.com>
2024-01-13 23:44:19 +0100

    progress

--- scripts/dialogs/dialog_1.gd
+++ scripts/dialogs/dialog_1.gd
@@ -12,7 +12,39 @@ var programme = \
 		'type': 'enemy_move_into_frame'
 	},
 	{
-		'type': 'player_dialog'
+		'type': 'player_dialog',
+		'theme': 'speech',
+		'content': 'KZZT'
+	},
+	{
+		'type': 'player_dialog',
+		'theme': 'speech',
+		'content': 'Neurons normalized.'
+	},
+	{
+		'type': 'player_dialog',
+		'theme': 'speech',
+		'content': 'BZZZZZT'
+	},
+	{
+		'type': 'player_dialog',
+		'theme': 'speech',
+		'content': 'Activating brain'
+	},
+	{
+		'type': 'player_dialog',
+		'theme': 'speech',
+		'content': 'BRRRRRR- beep'
+	},
+	{
+		'type': 'player_dialog',
+		'theme': 'speech',
+		'content': 'AAAAAAAAAAAAAAAAAAA, wait.'
+	},
+	{
+		'type': 'player_dialog',
+		'theme': 'speech',
+		'content': 'IT WORKED?'
 	}
 ]
 
