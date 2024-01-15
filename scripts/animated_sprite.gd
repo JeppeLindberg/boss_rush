fa78ec90c9cf790b75ede37bb2390d5b1ed2a5a9
unknown <jepp3441@gmail.com>
2024-01-15 22:37:14 +0100

    progress

--- scripts/animated_sprite.gd
+++ scripts/animated_sprite.gd
@@ -1,6 +1,8 @@
 extends Sprite2D
 
 @export var frames_per_second: float = 1.0
+@export var self_destroy: bool = false
+@export var random_rotation: bool = false
 @export var frames: Array[Texture]
 
 var _main_scene: Node2D
@@ -9,9 +11,14 @@ var _animation_begin: float
 
 func _ready():
 	_main_scene = get_node('/root/main_scene')
+	start_animation()
 
 func _process(_delta):
 	var _animation_progress = int((_main_scene.curr_secs() - _animation_begin) * frames_per_second)
+
+	if self_destroy and (_animation_progress > len(frames) - 1):
+		queue_free();
+		return;
 
 	_animation_progress = _animation_progress % len(frames)
 
@@ -20,4 +27,7 @@ func _process(_delta):
 func start_animation():
 	_animation_begin = _main_scene.curr_secs()
 	texture = frames[0]
+
+	if random_rotation:
+		rotation_degrees = [0.0, 90.0, 180.0, 270.0].pick_random()
 
